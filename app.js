22f67a7ae82703cbe9b83a821f18b9145ff50610
Md Rabbani <talk2mdrabbani@gmail.com>
2023-07-16 12:03:15 +0530

    Added mongodb cloud database feature

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 //jshint esversion:6
-//git verson-2.0 
+//git verson-2.0
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
@@ -8,15 +9,22 @@ const { pseudoRandomBytes } = require("crypto");
 const _ = require("lodash");
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://127.0.0.1:27017/blogDB", {useNewUrlParser: true});
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.DB_HOST);
+    console.log("Connected to MongoDB database " + conn.connection.host);
+  } catch (e) {
+    console.log("Error in MongoDB " + e);
+  }
+};
+connectDB();
 
 const postSchema = {
   title: String,
-  content: String
-
+  content: String,
 };
 
-const Post = mongoose.model("Post",postSchema);
+const Post = mongoose.model("Post", postSchema);
 
 const homeStartingContent =
   "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -26,8 +34,6 @@ const contactContent =
   "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 const app = express();
 
-
-
 app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -36,14 +42,12 @@ app.use(express.static("public"));
 // let posts = [];
 
 app.get("/", function (req, res) {
-  
-  Post.find({},function(err, posts){
+  Post.find({}, function (err, posts) {
     res.render("home", {
       homeStartingContent: homeStartingContent,
       posts: posts,
     });
   });
-
 });
 
 app.get("/about", function (req, res) {
@@ -65,39 +69,38 @@ app.post("/compose", function (req, res) {
   });
 
   // posts.push(post);
-  post.save(function(err){
-    if(!err){
+  post.save(function (err) {
+    if (!err) {
       res.redirect("/");
     }
   });
 
-  res.redirect("/");
+  // res.redirect("/");
 });
 
 app.get("/posts/:postId", function (req, res) {
-
   // const requestedTitle = req.params.PostName;
 
   // Post.forEach(function (post) {
-    // const storedTitle = Post.title;
+  // const storedTitle = Post.title;
 
-    // if (_.lowerCase([storedTitle]) == _.lowerCase([requestedTitle])) {
-    //   res.render("post", {
-    //     title: post.title,
-    //     content: post.content,
-    //   });
-    // } else {
-    //   console.log("No Match Found");
-    // }
+  // if (_.lowerCase([storedTitle]) == _.lowerCase([requestedTitle])) {
+  //   res.render("post", {
+  //     title: post.title,
+  //     content: post.content,
+  //   });
+  // } else {
+  //   console.log("No Match Found");
+  // }
 
-    const requestedPostId = req.params.postId;
-    
-    Post.findOne({_id: requestedPostId}, function(err, post){
-      res.render("post",{
-        title: post.title,
-        content: post.content
-      });
+  const requestedPostId = req.params.postId;
+
+  Post.findOne({ _id: requestedPostId }, function (err, post) {
+    res.render("post", {
+      title: post.title,
+      content: post.content,
     });
+  });
   // });
 });
 
